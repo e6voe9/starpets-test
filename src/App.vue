4b05d4d91bae06909437ea229e828ef47156b4c1
e6voe9 <george.bakhanovych@gmail.com>
2021-12-26 22:41:04 +0200

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minima repellat laboriosam pariatur esse asperiores officia ullam quia rerum totam obcaecati consectetur debitis accusantium est velit amet dolore, delectus temporibus.
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  components: {
-  }
-}
-</script>
-
 <style lang="scss">
-  #app{
-    background: #333;
-    &:hover{
-      background: #000;
+#app {
+  font-family: monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
+}
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    Home page
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
