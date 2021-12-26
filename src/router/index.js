6b436a5864956e6640000cbd164bc91bd847effe
e6voe9 <george.bakhanovych@gmail.com>
2021-12-26 23:19:31 +0200

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import(/* webpackChunkName: "notfound" */ '@/views/NotFound.vue'),
+  }, {
+    path: '*',
+    redirect: '/404'
   }
 ]
 
