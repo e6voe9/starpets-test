67d2863a0f30032b5d7d241355675c223f13290b
e6voe9 <george.bakhanovych@gmail.com>
2021-12-26 22:33:42 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis minima repellat laboriosam pariatur esse asperiores officia ullam quia rerum totam obcaecati consectetur debitis accusantium est velit amet dolore, delectus temporibus.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  #app{
+    background: #333;
+    &:hover{
+      background: #000;
+    }
+  }
+</style>
